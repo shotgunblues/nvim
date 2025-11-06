4ba10269730aa801b3992aca7a2486be50de2983
shotgunblues <gobackto.v8@gmail.com>
2025-11-06 14:04:30 +0300

    Add diagnostic info

--- lua/core/general.lua
+++ lua/core/general.lua
@@ -1,3 +1,9 @@
 vim.g.mapleader = " "
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+vim.diagnostic.config({
+    virtual_text = true,
+    sings = true,
+    update_in_insert = false,
+})
